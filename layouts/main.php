c4ef2eb1d8d356c21c260aa03349e50fec898a1e
Toby Sullivan <tobyjsullivan@gmail.com>
2012-12-03 21:43:54 -0800

    Added feedbackify

--- layouts/main.php
+++ layouts/main.php
@@ -28,6 +28,13 @@
 	<?php
 	echo $this->fetch('script');
 	?>
+	
+	<script type="text/javascript">
+	document.write('<scr' + 'ipt src="' + document.location.protocol + '//fby.s3.amazonaws.com/fby.js?100"></scr' + 'ipt>');
+	</script>
+	<script type="text/javascript">
+	FBY.showTab({id: '3494', position: 'right', color: '#2E88D1'});
+	</script>
 
 	<title><?= $this->fetch('page_title') ?> - <?= Configure::read('Company.name') ?></title>
 </head>
