79616a8e5ec2680978d9c9717a81f8705ecc57e0
Toby Sullivan <tobyjsullivan@gmail.com>
2012-12-09 18:07:46 -0800

    Added Google analytics tracking code

--- layouts/main.php
+++ layouts/main.php
@@ -37,6 +37,27 @@
 	</script>
 
 	<title><?= $this->fetch('page_title') ?> - <?= Configure::read('Company.name') ?></title>
+
+	<?php
+	if(Configure::read('Instance.debug') == 0) {
+		?>
+		<script type="text/javascript">
+
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-36928143-1']);
+		_gaq.push(['_setDomainName', 'indieclasses.com']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+		</script>
+		<?php
+	}
+	?>
 </head>
 <body>
 	<div class="container">
