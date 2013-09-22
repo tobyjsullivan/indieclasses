1cdf433daed209da4007b1cd22f100a484a51497
Toby Sullivan <tobyjsullivan@gmail.com>
2013-09-22 11:45:34 -0700

    Renamed src directory to www

--- src/install_mail.sh
+++
@@ -1,3 +0,0 @@
-sudo pear install Mail
-sudo pear install Net_SMTP
-sudo pear install Mail_Mime
---
+++ www/install_mail.sh
@@ -0,0 +1,3 @@
+sudo pear install -s Mail
+sudo pear install -s Net_SMTP
+sudo pear install -s Mail_Mime
